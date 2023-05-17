59d8aa835568e6194f05df4f90801e287c4e2101
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-05-17 15:24:29 +0530

    Added Middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.listen(3000, () => {
-  console.log("Kunal is here");
+app.get("/", (req, res) => {
+  res.json({
+    message: "goodbye world",
+  });
+});
+
+app.use((req, res, next) => {
+  const err = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "⭐" : err.stack,
+  });
+});
+
+app.listen(1337, () => {
+  console.log("Server started");
 });
