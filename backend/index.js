c8c2f97814e04075149dfdaa8c4a2187960fca62
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-09-12 15:42:56 +0530

    Schema creation

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const pinRoute = require("../backend/routes/pins");
+app.use(express.json());
+// console.log(process.env.REACT_APP_MONGO_URL);
+
+mongoose
+  .connect(process.env.REACT_APP_MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.log(err));
+
+app.post("/pins");
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
