2cf7728f630d1f41da16471f4e3fbb67285aac40
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-09-15 20:37:27 +0530

    axios set

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,15 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
+const cors = require("cors");
 app.use(express.json());
-// console.log(process.env.REACT_APP_MONGO_URL);
+app.use(cors());
 
+// console.log(process.env.REACT_APP_MONGO_URL);
+// app.get("/",  (req,res)=>{
+//   res.send("Helo");
+//   console.log("Req recieved");
+// })
 mongoose
   .connect(process.env.REACT_APP_MONGO_URL, {
     useNewUrlParser: true,
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
 import RoomIcon from "@mui/icons-material/Room";
 import StarIcon from "@mui/icons-material/Star";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./app.css";
+import axios from "axios";
 
 function App() {
   // const [showPopup, setShowPopup] = useState(true);
+  const [pins, setPins] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 17,
-    longitude: 46,
+    lat: 17,
+    long: 46,
     zoom: 8,
   });
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/api/pins");
+        setPins(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+  }, []);
   return (
     <>
       <Map
@@ -25,36 +39,40 @@ function App() {
         mapStyle="mapbox://styles/mapbox/light-v10"
         style={{ width: "100vw", height: "100vh" }}
       >
-        <Marker latitude={25.17} longitude={75.58} anchor="bottom">
-          <RoomIcon style={{ fontSize: viewport.zoom * 10 }} />
-        </Marker>
-        {/* {showPopup && ( */}
-        {/* <Popup
-          longitude={46}
-          latitude={17}
-          anchor="bottom"
-          // onClose={() => setShowPopup(false)}
+        {pins.map((p) => (
+          <>
+            <Marker latitude={p.lat} longitude={p.long} anchor="bottom">
+              <RoomIcon style={{ fontSize: viewport.zoom * 10 }} />
+            </Marker>
+            {/* {showPopup && ( */}
+            <Popup
+        latitude={p.lat}
+        longitude={p.long}
+        anchor="bottom"
+        // onClose={() => setShowPopup(false)}
         >
-          <div className="card">
-            <label>Place</label>
-            <h4 className="place">Kisi ka ghar</h4>
+        <div className="card">
+        <label>Place</label>
+        <h4 className="place">{p.title}</h4>
             <label>Review</label>
-            <p className="desc">Noice Place</p>
+            <p className="desc">{p.desc}</p>
             <label>Rating</label>
             <div className="stars">
-              <StarIcon className="star" />
-              <StarIcon className="star" />
-              <StarIcon className="star" />
-              <StarIcon className="star" />
-              <StarIcon className="star" />
+            <StarIcon className="star" />
+            <StarIcon className="star" />
+            <StarIcon className="star" />
+            <StarIcon className="star" />
+            <StarIcon className="star" />
             </div>
             <label>Information</label>
             <span className="username">
-              Created by <b>Fezaan</b>
+            Created by <b>{p.username}</b>
             </span>
             <span className="date">Two hours ago</span>
-          </div>
-        </Popup> */}
+            </div>
+          </Popup>
+          </>
+        ))}
         {/* )} */}
       </Map>
     </>
